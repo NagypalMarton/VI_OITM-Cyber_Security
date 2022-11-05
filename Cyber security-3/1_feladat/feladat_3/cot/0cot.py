524b0e03260027da8c2bbb90da96aee2dc0b050f
John Doe <johndoe@example.com>
2022-11-05 17:46:12 +0100

    15. sorban van a módosítás és jó lett volna

--- Cyber security-3/1_feladat/feladat_3/cot/0cot.py
+++ Cyber security-3/1_feladat/feladat_3/cot/0cot.py
@@ -3,17 +3,17 @@ BadChecksum=''
 for binName in checkFileBin:
     binName+=".bin"
     print(binName,end='\t')
-    bin=open(binName,"rb")
-    number=list(bin.read())
-    numberBytes=bytes(number)
-    bin.close()#fájlt 
-    binLength=len(number)
-    binSUM=0
-    for x in range(binLength-1):#utolsó elem (checksum) előttiig
-        binSUM=binSUM+number[x]
-    newChecksum=binSUM&255
-    if newChecksum !=number[binLength-1]:
-        BadChecksum+=binName+" "
-    if number[0]:#első elem (pointer)
-        checkFileBin.append(chr(number[0]))#Sámból ASCII kóddá válik, majd a listához adja
+    file=open(f'D:\Projects\VI_OITM-Cyber_Security\Cyber security-3\\1_feladat\\feladat_3\cot\{binName}',"rb")
+    content=list(file.read())
+    contentBytes=bytes(content)
+    file.close()#fájlt 
+    fileLength=len(content)
+    checksum=0
+    for x in range(fileLength-1):#utolsó elem (checksum) előttiig
+        checksum=checksum+content[x]
+    checksum=checksum&255
+    if checksum ==content[fileLength-1]: #!=
+        BadChecksum+=binName[0]+" "
+    if content[0]:#első elem (pointer)
+        checkFileBin.append(chr(content[0]))#Számból ASCII kóddá válik, majd a listához adja
 print("\nHelytelen BIN: \n\t"+BadChecksum)
