17444a709cd0f1c37870e7b63198d9e91460a158
John Doe <johndoe@example.com>
2022-11-11 14:42:31 +0100

    4es megoldás

--- Cyber security-4/1_feladat/4_feladat/RSA_PSS.py
+++ Cyber security-4/1_feladat/4_feladat/RSA_PSS.py
@@ -1,42 +1,24 @@
-#Adott mondat: Continental - The Future in Motion
-#Hash -> SHA256
-#RSA key hossz: 2048bit
-#kulcspár: .pem
-#RSA PSS aláírás
-#Add meg azt a RSA PSS aláírást, amelyik a fenti jelmondatra lett generálva!
+# Adott mondat: Continental - The Future in Motion
+# Hash -> SHA256
+# RSA key hossz: 2048bit
+# kulcspár: .pem
+# RSA PSS aláírás
+# Add meg azt a RSA PSS aláírást, amelyik a fenti jelmondatra lett generálva!
 
-# from Crypto.PublicKey import RSA
-# from Crypto.Random import get_random_bytes
-# from Crypto.Cipher import AES, PKCS1_OAEP
-
-# data = "Continental - The Future in Motion".encode("utf-8")
-# file_out = open("encrypted_data.bin", "wb")
-
-# recipient_key = RSA.import_key(open("D:\Projects\VI_OITM-Cyber_Security\Cyber security-4\\1_feladat\\4_feladat\public_key.pem").read())
-# session_key = get_random_bytes(16)
-
-# # A munkamenetkulcs titkosítása a nyilvános RSA-kulccsal
-# cipher_rsa = PKCS1_OAEP.new(recipient_key)
-# enc_session_key = cipher_rsa.encrypt(session_key)
-
-# # Az adatok titkosítása az AES munkamenetkulccsal
-# cipher_aes = AES.new(session_key, AES.MODE_EAX)
-# ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-# [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-# file_out.close()
-# print(enc_session_key)
 from Crypto.Signature import pss
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
-from Crypto import Random
 
-message = 'Continental - The Future in Motion'
+message = b'Continental - The Future in Motion'
+public_key = RSA.import_key(open('D://Projects//VI_OITM-Cyber_Security-1//Cyber security-4//1_feladat//4_feladat//public_key.pem').read())
+
+option_sign=open('D://Projects//VI_OITM-Cyber_Security-1//Cyber security-4//1_feladat//4_feladat//option_1_sign.txt')
+signature=bytes.fromhex(option_sign.read())
+
+h = SHA256.new(message)
+verifier=pss.new(public_key)
 try:
-    key = RSA.import_key(open('D:\Projects\VI_OITM-Cyber_Security\Cyber security-4\\1_feladat\\4_feladat\private_key.pem').read())
-    h = SHA256.new(message)
-    signature = pss.new(key).sign(h)
-    print(signature)
-except (ValueError, TypeError):
-    print(f"Hiba!\n{ValueError.args} - {TypeError}")
-for x in range(4):
-    print(x)
+    verifier.verify(h,signature)
+    print("Az aláírás jó!")
+except(ValueError,TypeError):
+    print('Az aláírás nem jó!')
